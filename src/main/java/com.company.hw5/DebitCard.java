2035d5b37002db45865e6da1a8e8c222d3678f01
mmalashchonak <mmalashchonak@gmail.com>
2020-09-16 00:02:19 +0300

    hw5 done

--- src/main/java/com.company.hw5/DebitCard.java
+++ src/main/java/com.company.hw5/DebitCard.java
@@ -2,18 +2,22 @@ package com.company.hw5;
 
 public class DebitCard extends Card {
 
-    DebitCard(String name, double balance){
+    DebitCard(String name, double balance) {
         super(name, balance);
     }
 
-    DebitCard(String name){
+    DebitCard(String name) {
         super(name);
     }
 
     @Override
     public void getMoneyFromBalance(double sum) {
-        if(balance - sum < 0) return;
-        balance -= sum;
+        if (balance - sum < 0) {
+            throw new RuntimeException("Not enough money for this operation.");
+        }
+        else {
+            balance -= sum;
+        }
     }
 
 
