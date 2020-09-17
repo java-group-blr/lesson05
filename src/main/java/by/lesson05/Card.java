80f4aff2d4947052d7b43210a5aca84ea672d238
Siarhei Charepka <sergei1770984@gmail.com>
2020-09-17 13:07:03 +0300

    last v

---
+++ src/main/java/by/lesson05/Card.java
@@ -0,0 +1,36 @@
+package by.lesson05;
+
+public abstract class Card {
+    String userName;
+    double account;
+
+    public Card(String userName, double account) {
+        this.userName = userName;
+        this.account = account;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public void setAccount(double account) {
+        this.account = account;
+    }
+
+    abstract double getCardBalance();
+
+    abstract double addMoney(double money);
+
+    abstract double getMoney(double money);
+
+    abstract double changeMoney(double course);
+
+}
